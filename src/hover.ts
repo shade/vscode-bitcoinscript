c150bf04bc8c2c0a02a7254bf769645e4cd311b9
Joe Thomas <josepht404@gmail.com>
2020-07-26 12:29:08 -0700

    Fix the formatter stuff

--- src/hover.ts
+++ src/hover.ts
@@ -4,7 +4,7 @@ import * as yaml from 'js-yaml';
 import * as fs from 'promise-fs';
 
 interface IDefinition {
-    opcode: string,
+    token: string,
     dec: string,
     hex: string,
     description: string,
@@ -13,54 +13,117 @@ interface IDefinition {
     disabled?: boolean,
 }
 
+const DEC_REPLACE_REGEX = /\\\[\\\[dec\\\]\\\]/g;
+const OUTPUT_REPLACE_REGEX = /\\\[\\\[output\\\]\\\]/g;
+
 export default class HoverProvider implements vscode.HoverProvider {
-    _opcodeMap: Record<string, IDefinition>;
+    _opcodeMap: Record<string, vscode.MarkdownString[]>;
 
     constructor(defLocation: string) {
         this._opcodeMap = {};
 
         fs.readFile(defLocation, 'utf8')
             .then(contents => {
-                let doc = yaml.safeLoad(contents)
-                doc.forEach((def: IDefinition) => {
-                    def.opcode.split('|')
-                        .forEach(opcode => {
-                            this._opcodeMap[opcode] = def
-                        })
-                })
+                let doc = yaml.safeLoad(contents);
+
+                doc.forEach(def => this.addDefinition(def));
             });
+    }
+
+    private replacePlacholders(input: string, dec: string, output: string): string {
+        return input.replace(OUTPUT_REPLACE_REGEX, output)
+            .replace(DEC_REPLACE_REGEX, dec);
+    }
+
+    private addDefinition(def: IDefinition) {
+        let tokens = def.token.split('|');
+        let decs = def.dec.split('|');
+        let hexs = def.hex.split('|');
+
+        let outputs = [""];
+        
+        if (def.output) {
+            outputs = def.output.split('|');
+        }
+
+        let len = tokens.length;
+
+        if ((hexs.length != len)
+            || (decs.length != len)
+            || (outputs.length != len && (outputs.length != 1))) {
+            throw new Error("Invalid definitions file!");
+        }
+
+        if (outputs.length != len) {
+            outputs = new Array(len).fill(outputs[0]);
+        }
+
+        for (let i = 0; i < len; i++) {
+            let token = tokens[i];
+            let dec = decs[i];
+            let hex = hexs[i];
+            let output = outputs[i];
+
+            let card = [
+                this.makeHeader(token),
+                this.makeRawValues(dec, hex),
+            ];
+
+            if (def.input && def.output) {
+                card.push(this.makeInputOutput(def.input, output, dec));
+            }
+
+            card.push(this.makeDescription(def.description, dec, output));
+
+            if (def.disabled) {
+                card.push(this.makeDisabled());
+            }
+
+            this._opcodeMap[token] = card;
+        }
+    }
+
+    private makeHeader(token: string): vscode.MarkdownString {
+        let header = new vscode.MarkdownString("");
+        header.appendCodeblock(token, 'bitcoinscript');
+
+        return header;
+    }
+
+    private makeDescription(description: string, dec: string, output: string): vscode.MarkdownString {
+        let desc = new vscode.MarkdownString(
+            this.replacePlacholders(description, dec, output)
+        );
+
+        desc.appendMarkdown('\n\n[Visit Wiki](https://wiki.bitcoinsv.io/index.php/Opcodes_used_in_Bitcoin_Script)');
+
+        return desc;
+    }
+
+    private makeRawValues(dec: string, hex: string): vscode.MarkdownString {
+        return new vscode.MarkdownString(`Raw \`${dec}\` \`0x${hex}\``);
+    }
+
+    private makeInputOutput(input: string, output: string, dec: string): vscode.MarkdownString {
+        let inp = this.replacePlacholders(input, dec, output);
+        let out = this.replacePlacholders(output, dec, output);
+
+        return new vscode.MarkdownString(`input **${inp}**\n\noutput **${out}**`);
+    }
+
+    private makeDisabled(): vscode.MarkdownString {
+        return new vscode.MarkdownString('**DISABLED in Bitcoin SV**')
     }
 
     provideHover(document: vscode.TextDocument, position: vscode.Position, cancelToken: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
         let word = document.getWordRangeAtPosition(position);
         let token = document.getText(word);
 
-        let definition = this._opcodeMap[token];
-        if (!definition) {
+        let card = this._opcodeMap[token];
+        if (!card) {
             return null;
         }
 
-        let { description, hex, dec, input, output, disabled } = definition;
-
-        let header = new vscode.MarkdownString("");
-        header.appendCodeblock(token, 'bitcoinscript');
-
-        let body = new vscode.MarkdownString(description);
-        body.appendMarkdown('\n\n[Visit Wiki](https://wiki.bitcoinsv.io/index.php/Opcodes_used_in_Bitcoin_Script)');
-
-        let msg = [
-            header,
-            new vscode.MarkdownString(`Raw \`${dec}\` \`${hex}\``),
-        ];
-
-        if (input && output) {
-            msg.push(new vscode.MarkdownString(`input **${input}**\n\noutput **${output}**`));
-        }
-        msg.push(body)
-        if (disabled) {
-            msg.push(new vscode.MarkdownString('**DISABLED in Bitcoin SV**'))
-        }
-
-        return new vscode.Hover(msg);
+        return new vscode.Hover(card);
     }
 }
